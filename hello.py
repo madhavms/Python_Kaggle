852a137d27e0205654b891c5f504d20ba6f4d84e
MADHAV MANOHAR <madhav.ms@inapp.com>
2019-04-21 20:53:38 +0530

    initial commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+spam_amount = 0
+print(spam_amount)
+
+# Ordering Spam, egg, Spam, Spam, bacon and Spam (4 more servings of Spam)
+spam_amount = spam_amount + 4
+
+if spam_amount > 0:
+    print("But I don't want ANY spam!")
+# SpamSpamSpamSpam is the output. ie multiplying a string with a number,multiplies the string
+viking_song = "Spam " * spam_amount
+print(viking_song)
